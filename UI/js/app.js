db994e27780c9c04208758119daa9f2ad87af6f9
Tiku Okoye <okoyetiku@gmail.com>
2018-11-01 10:11:59 +0100

    Create Dashboard pages and modals

--- UI/js/app.js
+++ UI/js/app.js
@@ -1,8 +1,38 @@
-let toggler = document.getElementById('toggle');
-let menu = document.getElementById('menu');
-let dashboard = document.getElementById('dash');
 
-toggler.onclick = () => {
-  menu.style.display = 'block';
-  menu.classList.add('toggled-nav');
+let myDocument = $(document);
+
+let deviceWidth = myDocument.width();
+
+
+if (deviceWidth <= 767) {
+  $(document).click(function(){
+    $('aside').fadeOut(500);
+  })
+  $(document).on('click', '#toggle', (event) => {
+    $('aside').fadeIn(500).addClass('toggled-nav');
+    event.stopPropagation();
+  })
+  $(document).on('click', 'aside', (event) => {
+    event.stopPropagation();
+  })
 }
+
+let parcelModal = document.getElementById('parcelmodal');
+let destinationModal = document.getElementById('destination-modal');
+let parcelBtn = document.getElementById('parcel-modal-open');
+let destinationBtn = document.getElementById('change-destination');
+let cancelBtn = document.getElementById('cancel-parcel')
+parcelBtn.onclick = () => Modal(parcelmodal); 
+destinationBtn.onclick = () => Modal(destinationModal);
+
+
+
+function Modal(modalId) {
+  modalId.style.display = 'block';
+  window.onclick = function(event) {
+  if (event.target == modalId) {
+    modalId.style.display = "none";
+  }
+}
+}
+
