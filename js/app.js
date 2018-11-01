8e36296002858e033526531cc32112b6be5ad0d6
Tiku Okoye <okoyetiku@gmail.com>
2018-11-01 16:03:20 +0100

    Add functionality to the modals

--- js/app.js
+++ js/app.js
@@ -1,9 +1,8 @@
 
-let myDocument = $(document);
+let myDocument = $(document),
+    deviceWidth = myDocument.width();
 
-let deviceWidth = myDocument.width();
-
-
+// To hide and dsiplay side menu
 if (deviceWidth <= 767) {
   $(document).click(function(){
     $('aside').fadeOut(500);
@@ -17,22 +16,47 @@ if (deviceWidth <= 767) {
   })
 }
 
-let parcelModal = document.getElementById('parcelmodal');
-let destinationModal = document.getElementById('destination-modal');
-let parcelBtn = document.getElementById('parcel-modal-open');
-let destinationBtn = document.getElementById('change-destination');
-let cancelBtn = document.getElementById('cancel-parcel')
-parcelBtn.onclick = () => Modal(parcelmodal); 
-destinationBtn.onclick = () => Modal(destinationModal);
+// Modals
+let parcelModal = document.getElementById('parcelmodal'),
+    cancelModal = document.getElementById('cancel-modal'),
+    destinationModal = document.getElementById('destination-modal'),
+    profileModal = document.getElementById('profile-modal'),
+    locationModal = document.getElementById('location-modal'),
+    updateModal = document.getElementById('update-modal');
+
+// Buttons
+let parcelBtn = document.getElementById('parcel-modal-open'),
+    parcelBtn2 = document.getElementById('parcel-modal'),
+    cancelBtn = document.getElementById('cancel-parcel'),
+    profileBtn = document.getElementById('profile-btn')
+    destinationBtn = document.getElementById('change-destination'),
+    locationBtn = document.getElementById('change-location'),
+    updateBtn = document.getElementById('update-status');
 
 
+Validate(parcelBtn, parcelModal);
+Validate(parcelBtn2, parcelModal);
+Validate(cancelBtn, cancelModal);
+Validate(profileBtn, profileModal);
+Validate(destinationBtn, destinationModal);
+Validate(locationBtn, locationModal);
+Validate(updateBtn, updateModal);
 
+// Function to validate if element is on the DOM and call Modal function
+function Validate(btn, modal) {
+  if (btn !== null) {
+    btn.onclick = () => Modal(modal);
+  }
+}
+
+
+// Function to open and close modal
 function Modal(modalId) {
   modalId.style.display = 'block';
   window.onclick = function(event) {
-  if (event.target == modalId) {
-    modalId.style.display = "none";
+    if (event.target == modalId) {
+      modalId.style.display = "none";
+    }
   }
-}
 }
 
