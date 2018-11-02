0ecf2707877a32549c018d5e765811b102b749f6
Tiku Okoye <okoyetiku@gmail.com>
2018-11-02 13:00:46 +0100

    Fix mobile responsiveness and side menu hide/show functionality

--- js/app.js
+++ js/app.js
@@ -1,89 +1,70 @@
 window.addEventListener('load', () => {
-  resize();
+    resize();
+    modalSetup();
 })
 
 window.addEventListener('resize', function() {
-  resize();
+    resize();
 })
 
 const $ = (selector) => {
-  return document.querySelector(selector)
+    return document.querySelector(selector)
 };
 
-var viewportwidth;
-var viewportheight;
-
+// Function to hide and show side menu
 function resize() {
-  let
-    deviceWidth = window.innerWidth;
-    console.log(deviceWidth);
-    console.log(typeof window.innerWidth);
-  if (typeof window.innerWidth == 'number') {
-    viewportwidth = window.innerWidth,
-    viewportheight = window.innerHeight
-    console.log(viewportwidth)
-    if(viewportwidth <= 700) {
-      window.reload;
-      }
-  }
+    let deviceWidth = window.innerWidth;
+    let aside = $('aside');
+    let asideClass = aside.classList;
 
-  let aside = $('aside');
-  let asideClass = aside.classList;
-  
-  // To hide and dsiplay side menu
-  if (deviceWidth <= 767) {
-    
-    $('#main-content-page').addEventListener('click', () => {
-      // asideClass.add('fade-out');
-      asideClass.remove('fade-in');
-    });
-     
-    $('#toggle').addEventListener('click', (event) => {
-      console.log('am clicking on toggle');
-      
-      asideClass.add('fade-in')
-      asideClass.toggle('toggled-nav');
-      
-      event.stopPropagation();
-    });
-  }
+    if (deviceWidth <= 767) {
+        $('#main-content-page').addEventListener('click', () => {
+            asideClass.remove('fade-in');
+            asideClasslass.remove('toggled-nav')
+        });
+
+        $('#toggle').addEventListener('click', () => {
+            asideClass.add('fade-in')
+            asideClass.add('toggled-nav');
+        });
+    }
 }
 
 // Function to validate if element is on the DOM and call Modal function
 function validate(btn, modal) {
-  if (btn !== null) {
-    btn.onclick = () => toggleModal(modal);
-  }
+    if (btn !== null) {
+        btn.onclick = () => toggleModal(modal);
+    }
 }
-
 
 // Function to open and close modal
 function toggleModal(modalElement) {
-  modalElement.style.display = 'block';
-  modalElement.show = function() {    
-    this.style.display = "block";
-  }
-  modalElement.hide = function() {
-    this.style.display = "none";
-  }
-  
-  const buttons = modalElement.querySelectorAll('button[type=submit]');
-  for(let button of buttons) {
-    button.addEventListener("click", () => {
-      modalElement.hide();
-    });
-  }
-  window.onclick = function(event) {
-    if (event.target == modalId) {
-      modalElement.show();
+    modalElement.style.display = 'block';
+    modalElement.show = function() {
+        this.style.display = "block";
     }
-  }
+    modalElement.hide = function() {
+        this.style.display = "none";
+    }
+    const buttons = modalElement.querySelectorAll('button[type=submit]');
+    for (let button of buttons) {
+        button.addEventListener("click", () => {
+            modalElement.hide();
+        });
+    }
+    window.onclick = function(event) {
+        if (event.target == modalElement) {
+            modalElement.hide();
+        }
+    }
 }
 
-const buttons = document.querySelectorAll('[data-modal]');
+// Funtion to get all Modal Buttons and their respective modals
+function modalSetup() {
+    const modalButtons = document.querySelectorAll('[data-modal]');
 
-for(let button of buttons) {
-  let targetModal = button.getAttribute('data-target');
-  
-  validate(button, $(targetModal));
+    for (let button of modalButtons) {
+        let targetModal = button.getAttribute('data-target');
+        validate(button, $(targetModal));
+    }
 }
