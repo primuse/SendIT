1141cd28a25a0f3e51897043642ad369b7e19da4
Tiku Okoye <okoyetiku@gmail.com>
2018-11-07 18:54:16 +0100

    Add a Procfile

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import userRoute from './routes/userRoute';
 import parcelRoute from './routes/parcelRoute';
-import { Server } from 'https';
 
 const app = express();
-const port = 3000;
+let port = process.env.PORT;
 
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
@@ -13,10 +12,12 @@ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 app.use('/api/v1', userRoute);
 app.use('/api/v1', parcelRoute);
 
-app.get('/', (req, res) => res.send('Welcome to SendIT!'));
+if (port == null || port === '') {
+  port = 3000;
+}
 
 const server = app.listen(port, () => {
-  console.log('server started successfully!');
+  console.log(`Server started on PORT ${port}`);
 });
 
 export default server;
