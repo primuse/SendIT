bee268bf06f2a4c1d55fb7f6c2b50ac6acf9fdfe
Tiku Okoye <okoyetiku@gmail.com>
2018-11-16 11:08:09 +0100

    [Feature #161961156] Create New Parcel API endpoint

--- src/routes/parcelRoute.js
+++ src/routes/parcelRoute.js
@@ -7,7 +7,8 @@
 * @requires ValidateMiddleware
 */
 import express from 'express';
-import ParcelController from '../controllers/dbParcelController';
+import DbParcelController from '../controllers/dbParcelController';
+import ParcelController from '../controllers/ParcelController';
 import ValidateMiddleware from '../middleware/validator';
 
 const router = express.Router();
@@ -15,41 +16,46 @@ const router = express.Router();
 /**
 * Route to create new Parcel Orders
 * @param  {string} route The Post url route
-* @param  {function} ParcelController.createParcels The handler method
+* @param  {function} DbParcelController.createParcels The handler method
 * @returns {(obj|obj} success message or error message
 */
-router.post('/parcels', ValidateMiddleware.validateParcel, ParcelController.createParcels);
+router.post('/auth/parcels', ValidateMiddleware.validateParcel, DbParcelController.createParcels);
 
-// /**
-// * Route to get all Parcel Orders
-// * @param  {string} route The Get Parcels url route
-// * @param  {function} ParcelController.getAllParcels The handler function
-// * @returns {(obj|obj} parcel or error message
-// */
-// router.get('/parcels', ParcelController.getAllParcels);
+/**
+ * with dummy Json file
+ */
+router.post('/parcels', ParcelController.createParcels);
 
-// /**
-// * Route to get parcel order by ID
-// * @param  {string} route The Get parcels/:parcelID url route
-// * @param  {function} ParcelController.getParcel The handler function
-// * @returns {(obj|obj} parcel or error message
-// */
-// router.get('/parcels/:parcelId', ParcelController.getParcel);
+/**
+* Route to get all Parcel Orders
+* @param  {string} route The Get Parcels url route
+* @param  {function} ParcelController.getAllParcels The handler function
+* @returns {(obj|obj} parcel or error message
+*/
+router.get('/parcels', ParcelController.getAllParcels);
 
-// /**
-// * Route to Update a Parcel
-// * @param  {string} route The Update url route
-// * @param  {function} ParcelController.updateParcel The handler function
-// * @returns {(obj|obj} success message or error message
-// */
-// router.put('/parcels/:parcelId/update', ParcelController.updateParcel);
+/**
+* Route to get parcel order by ID
+* @param  {string} route The Get parcels/:parcelID url route
+* @param  {function} ParcelController.getParcel The handler function
+* @returns {(obj|obj} parcel or error message
+*/
+router.get('/parcels/:parcelId', ParcelController.getParcel);
 
-// /**
-// * Route to cancel a parcel order
-// * @param  {string} route The cancel url route
-// * @param  {function} ParcelController.cancelParcel The handler function
-// * @returns {(obj|obj} success message or error message
-// */
-// router.put('/parcels/:parcelId/cancel', ParcelController.cancelParcel);
+/**
+* Route to Update a Parcel
+* @param  {string} route The Update url route
+* @param  {function} ParcelController.updateParcel The handler function
+* @returns {(obj|obj} success message or error message
+*/
+router.put('/parcels/:parcelId/update', ParcelController.updateParcel);
+
+/**
+* Route to cancel a parcel order
+* @param  {string} route The cancel url route
+* @param  {function} ParcelController.cancelParcel The handler function
+* @returns {(obj|obj} success message or error message
+*/
+router.put('/parcels/:parcelId/cancel', ParcelController.cancelParcel);
 
 export default router;
