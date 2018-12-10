6a74f8a01eb9c6c9144f886c7108354feaea466a
Tiku Okoye <okoyetiku@gmail.com>
2018-12-10 12:00:32 +0100

    Implement Signup functionality

--- UI/js/main.js
+++ UI/js/main.js
@@ -19,6 +19,7 @@ class Fetch {
 		.then(handleErrors)
 		.then(res =>  { 
 			localStorage.setItem('token', res.data[0].token);
+			localStorage.setItem('id', res.data[0].user.id);
 			window.location = 'dashboard.html';
 		})
 		.catch((err) => {
@@ -26,42 +27,49 @@ class Fetch {
 		})
 	}
 
-	// static signup() {
-	// 	const firstName = document.getElementById('firstName').value;
-	// 	const lastName = document.getElementById('lastName').value;
-	// 	const email = document.getElementById('email').value;
-	// 	const password = document.getElementById("password").value;
-	// 	const myData = {
-	// 		firstName,
-	// 		lastName,
-	// 		email,
-	// 		password,
-	// 	};
-	// 	const config = {
-	// 		method: 'POST',
-	// 		headers: new Headers({
-	// 			'Content-Type': 'application/json'
-	// 		}),
-	// 		body: JSON.stringify(myData),
-	// 	};
+	static signup(event) {
+		event.preventDefault();
+		const firstName = document.getElementById('firstName').value;
+		const lastName = document.getElementById('lastName').value;
+		const email = document.getElementById('email').value;
+		const password = document.getElementById("password").value;
+		const myData = {
+			firstName,
+			lastName,
+			email,
+			password,
+		};
+		const config = {
+			method: 'POST',
+			headers: new Headers({
+				'Content-Type': 'application/json'
+			}),
+			body: JSON.stringify(myData),
+		};
 
-	// 	fetch('http://localhost:3000/api/v1/auth/signup', config)
-	// 	.then(res => res.json())
-	// 	.then(res => console.log(res))
-	// }
+		fetch('http://localhost:3000/api/v1/auth/signup', config)
+		.then(handleErrors)
+		.then(res =>  { 
+			localStorage.setItem('token', res.data.token);
+			localStorage.setItem('id', res.data.user.id);
+			window.location = 'new_dashboard.html'
+		})
+		.catch((err) => {
+		  notif.make({text: 'Email already Registered', type: 'danger' });
+		})
+	}
 }
-	
-
-// // Implementing the signup page
-// let signupBtn = document.getElementById("signup");
-// if (signupBtn !== null) {
-// 	signupBtn.addEventListener('click', Fetch.signup);
-// }
 
 // Implementing the Login page
 let loginForm = document.getElementById('login-form');
 if (loginForm !== null) {
-	loginForm. addEventListener('submit', Fetch.login);
+	loginForm.addEventListener('submit', Fetch.login);
+}
+
+// Implementing the signup page
+let signupForm = document.getElementById("signupForm");
+if (signupForm !== null) {
+	signupForm.addEventListener('submit', Fetch.signup);
 }
 
 function handleErrors(res) {
